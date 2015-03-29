c3d29f188ee3c29f4efd687a834a000def6626cb
PuKoren <koren@pukogames.com>
2015-03-29 21:51:30 +0200

    Add combo

--- src/score.js
+++ src/score.js
@@ -1,33 +1,53 @@
 var RtodScore = (function() {
   var score = 0;
+  var combo = 1;
   var elementScope;
   var element = React.createClass({
     displayName: "RtodScoreSection",
     render: function() {
       return React.createElement("div", null,
         React.createElement('span', null, 'Score: '),
-        React.createElement('span', null, this.state.score)
+        React.createElement('span', null, this.state.score),
+        React.createElement('span', null,
+          React.createElement('span', null, 'x'),
+          React.createElement('span', null, this.state.combo)
+        )
       );
     },
     getInitialState: function() {
       elementScope = this;
       return {
-        score: 0
+        score: 0,
+        combo: 1
       };
     },
   });
+
+  var refreshDom = function() {
+    elementScope.setState({
+      score: score,
+      combo: combo
+    });
+  };
+
   return {
     increment: function() {
-      score++;
-      elementScope.setState({ score: score });
+      score += 1 * combo;
+      refreshDom();
     },
     decrement: function() {
-      score--;
-      elementScope.setState({ score: score });
+      score -= 1 * combo;
+      refreshDom();
+    },
+    wordComplete: function() {
+      score += 10 * combo;
+      combo++;
+      refreshDom();
     },
     reset: function() {
       score = 0;
-      elementScope.setState({ score: score });
+      combo = 0;
+      refreshDom();
     },
     getReactElement: function() {
       return element;
