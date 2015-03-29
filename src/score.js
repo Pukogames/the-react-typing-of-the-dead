80d821fc6416229c4d2093327d59002ac8ada5de
PuKoren <koren@pukogames.com>
2015-03-29 21:13:58 +0200

    Working increment/decrement/reset in score.js

--- src/score.js
+++ src/score.js
@@ -1,25 +1,36 @@
 var RtodScore = (function() {
   var score = 0;
+  var elementScope;
+  var element = React.createClass({
+    displayName: "RtodScoreSection",
+    render: function() {
+      return React.createElement("div", null,
+        React.createElement('span', null, 'Score: '),
+        React.createElement('span', null, this.state.score)
+      );
+    },
+    getInitialState: function() {
+      elementScope = this;
+      return {
+        score: 0
+      };
+    },
+  });
   return {
     increment: function() {
       score++;
+      elementScope.setState({ score: score });
     },
     decrement: function() {
       score--;
+      elementScope.setState({ score: score });
     },
     reset: function() {
       score = 0;
+      elementScope.setState({ score: score });
     },
     getReactElement: function() {
-      return React.createClass({
-        displayName: "RtodScoreSection",
-        render: function() {
-          return React.createElement("div", null,
-            React.createElement('span', null, 'Score: '),
-            React.createElement('span', null, score)
-          );
-        }
-      });
+      return element;
     }
   };
 })();
