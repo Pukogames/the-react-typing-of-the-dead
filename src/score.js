f84727732cc1a8f53964b1bca13d9561d7e330ad
PuKoren <koren@pukogames.com>
2015-03-29 21:05:53 +0200

    Modify score to have some functions

--- src/score.js
+++ src/score.js
@@ -1,9 +1,25 @@
-var RtodScore = React.createClass({displayName: "RtodScoreSection",
-  score: 0,
-  render: function() {
-    return React.createElement("div", null, 
-      React.createElement('span', null, 'Score: '),
-      React.createElement('span', null, this.score)
-    );
-  }
-});
+var RtodScore = (function() {
+  var score = 0;
+  return {
+    increment: function() {
+      score++;
+    },
+    decrement: function() {
+      score--;
+    },
+    reset: function() {
+      score = 0;
+    },
+    getReactElement: function() {
+      return React.createClass({
+        displayName: "RtodScoreSection",
+        render: function() {
+          return React.createElement("div", null,
+            React.createElement('span', null, 'Score: '),
+            React.createElement('span', null, score)
+          );
+        }
+      });
+    }
+  };
+})();
