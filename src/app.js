679e6eec05b9a05c4c2c51f154c58d7287f80643
Gaelle <pussy@pukogames.com>
2015-03-30 22:41:05 +0200

    GitHub page init

---
+++ src/app.js
@@ -0,0 +1,60 @@
+var RtodApp = React.createClass({displayName: 'RtodApp',
+  word: RtodDico.getRandomWord(),
+  getInitialState: function() {
+    return {
+      aim: this.word,
+      remaining: this.word
+    };
+  },
+  onKeyDown: function(e) {
+    if (e.target.value.length > 0 && this.word.indexOf(e.target.value) === 0) {
+      RtodScore.increment();
+      this.word = this.word.substring(e.target.value.length, this.word.length);
+      this.setState({
+        remaining: this.word
+      });
+      if (this.word.length === 0) {
+        RtodScore.wordComplete();
+        this.word = RtodDico.getRandomWord();
+        this.setState({
+          aim: this.word,
+          remaining: this.word
+        });
+      }
+    }
+    else {
+      RtodScore.decrement();
+    }
+    e.target.value = '';
+  },
+  render: function() {
+    return (
+      React.createElement('div', {className: 'RtodApp'},
+        React.createElement('h1', null, 'The React Typing of the Dead'),
+        React.createElement(RtodScore.getReactElement()),
+        React.createElement('div', {className: 'aim'},
+          React.createElement('div', {
+            dangerouslySetInnerHTML: {
+              __html: this.state.aim
+            }
+          }),
+          React.createElement('div', {
+            className: 'remaining',
+            dangerouslySetInnerHTML: {
+              __html: this.state.remaining
+            }
+          })
+        ),
+        React.createElement('input', {
+          onKeyUp: this.onKeyDown,
+          autoFocus: true
+        })
+      )
+    );
+  }
+});
+
+React.render(
+  React.createElement(RtodApp, null),
+  document.getElementById('content')
+);
---
+++ src/score.js
@@ -0,0 +1,54 @@
+var RtodScore = (function() {
+  var score = 0;
+  var combo = 1;
+  var elementScope;
+  var element = React.createClass({
+    displayName: "RtodScoreSection",
+    render: function() {
+      return React.createElement("div", null,
+        React.createElement('span', {className: 'score'},
+          React.createElement('span', null, this.state.score),
+          React.createElement('span', {className: 'combo'}, this.state.combo)
+        )
+      );
+    },
+    getInitialState: function() {
+      elementScope = this;
+      return {
+        score: 0,
+        combo: 1
+      };
+    },
+  });
+
+  var refreshDom = function() {
+    elementScope.setState({
+      score: score,
+      combo: combo
+    });
+  };
+
+  return {
+    increment: function() {
+      combo += 1;
+      refreshDom();
+    },
+    decrement: function() {
+      combo = 1;
+      refreshDom();
+    },
+    wordComplete: function() {
+      score += 10 * combo;
+      combo = 1;
+      refreshDom();
+    },
+    reset: function() {
+      score = 0;
+      combo = 1;
+      refreshDom();
+    },
+    getReactElement: function() {
+      return element;
+    }
+  };
+})();
